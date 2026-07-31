b5527373871ac6fb017bec99f40feb758b01ff91
hy Guo 郭慧源 <hy.guo@metpeak.com>
2026-07-31 13:52:48 +0800

    修改Day1代码

---
+++ Day1/iostream和输入输出/main.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+
+
+//'\n'和endl都可以实现换行，区别是endl还会强制刷新输出缓冲区
+int main()
+{
+	std::cout << "Hello, world!\n";
+	int score{ 25 };
+	std::cout << "Score" << score << "\n";
+	//  <<可以连续输出不同类型的数据
+	std::cout << "name = " << "Alice"
+		<< ", age = " << 20
+		<< '\n';
+
+	return 0;
+}
---
+++ Day1/命名空间/main.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+
+//命名空间：主要用于避免名称冲突
+
+//全引用 不建议大型项目头文件中使用
+//using namespace std;
+
+//单项引用
+using std::cout;
+
+
+int main()
+{
+    cout << "Hello, world!\n";
+
+    return 0;
+}
