872ae65138fa08916a21c6111e4b84a9a370c3a5
hy Guo 郭慧源 <hy.guo@metpeak.com>
2026-07-31 15:37:00 +0800

    增加暴力求解方法

--- Day1/Two_Sum/main.cpp
+++ Day1/Two_Sum/main.cpp
@@ -1,32 +1,52 @@
 ﻿#include <iostream>
 #include <vector>
 #include <unordered_map>
+//class Solution {
+//public:
+//    std::vector<int> twoSum(
+//        std::vector<int> &nums,
+//        int target
+//    ) {
+//        std::unordered_map <int, int > num_map;//存储数字及其索引
+//
+//		for (int i = 0;i < static_cast<int>(nums.size());++i)
+//		{
+//			const int complement = target - nums[i];
+//            const auto found = num_map.find(complement);
+//
+//            if (found != num_map.end())
+//            {
+//                return  { found->second,i };
+//            }
+//			num_map[nums[i]] = i;
+//			
+//		}
+//        return {};
+//
+//
+//    }
+//};
 class Solution {
 public:
     std::vector<int> twoSum(
         std::vector<int> &nums,
         int target
     ) {
-        std::unordered_map <int, int > num_map;//存储数字及其索引
-
-		for (int i = 0;i < static_cast<int>(nums.size());++i)
-		{
-			const int complement = target - nums[i];
-            const auto found = num_map.find(complement);
-
-            if (found != num_map.end())
+        for (int i = 0;i < (static_cast<int>(nums.size()));i++)
+        {
+            for (int j = i;j < (static_cast<int>(nums.size()));j++)
             {
-                return  { found->second,i };
+				if (nums[i] + nums[j] == target && i != j)
+				{
+					return { i,j };
+				}
             }
-			num_map[nums[i]] = i;
-			
-		}
-        return {};
+
+       }
 
 
     }
 };
-
 int main()
 {
     std::cout << "Hello, world!\n";
