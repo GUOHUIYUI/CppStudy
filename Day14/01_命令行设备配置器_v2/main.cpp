0f47f5844b0e34696427adb23f07c1f090313ff1
hy Guo 郭慧源 <hy.guo@metpeak.com>
2026-08-11 15:32:32 +0800

    完成Day14代码编写

--- Day14/01_命令行设备配置器_v2/main.cpp
+++ Day14/01_命令行设备配置器_v2/main.cpp
@@ -1,10 +1,197 @@
-#include <array>
+﻿#include <array>
 #include <cstdint>
 #include <iostream>
 #include <string>
 #include <string_view>
 
+enum class DeviceState {
+	offline,
+	idle,
+	running
+};
+
+enum class CommandResult {
+	ok,
+	emptyInput,
+	unknownCommand,
+	missingArgument,
+	invalidArgument
+};
+
+class DeviceConfig {
+public:
+	bool setName(std::string_view name)
+	{
+		if (name == "" || name.size() > 16)return false;
+		name_ = name;
+		return true;
+	}
+	bool setRate(int rate)
+	{
+		if (rate < 0 || rate>1000)return false;
+		rate_ = rate;
+		return true;
+	}
+	void setState(DeviceState state)
+	{
+		state_ = state;
+	}
+
+	const std::string &name() const
+	{
+		return name_;
+	}
+	int rate() const
+	{
+		return rate_;
+	}
+	DeviceState state() const
+	{
+		return state_;
+	}
+
+private:
+	std::string name_{ "device" };
+	int rate_{ 100 };
+	DeviceState state_{ DeviceState::offline };
+};
+
+bool parseRate(std::string_view text, int &rate)
+{
+	if (text == "") return false;
+
+	// 校验是否全为数字
+	for (char c : text)
+	{
+		if (c < '0' || c > '9')
+			return false;
+	}
+
+	int value = 0;
+	for (size_t i = 0; i < text.size(); i++)  // 从左往右
+	{
+		value = value * 10 + (text[i] - '0');
+		if (value > 1000) return false;
+	}
+
+	if (value > 0 && value < 1000)
+	{
+		rate = value;
+		return true;
+	}
+	return false;
+}
+
+
+std::string DeviceStateToStr(DeviceState state)
+{
+	switch (state)
+	{
+	case DeviceState::idle: return "idle";
+	case DeviceState::offline:return "offline";
+	case DeviceState::running: return "running";
+	default: return "unknown";
+	}
+}
+bool StrToDeviceState(std::string_view str, DeviceState &state)
+{
+	if (str == "idle")
+	{
+		state = DeviceState::idle;
+		return true;
+	}
+	if (str == "offline")
+	{
+		state = DeviceState::offline;
+		return true;
+	}
+	if (str == "running")
+	{
+		state = DeviceState::running;
+		return true;
+	}
+	return false;
+}
+
+std::string getLine()
+{
+	std::string line;
+	std::getline(std::cin, line);
+	return line;
+}
+
+void CommandComplier(std::string_view str, DeviceConfig &device)
+{
+	std::string command{};
+	std::string value{};
+	size_t index = str.find(' ');
+	if (index == std::string::npos)
+	{
+		command = str;
+	}
+	else
+	{
+		command = str.substr(0, index);
+		value = str.substr(index + 1);
+	}
+	if (command == "SHOW")
+	{
+		std::cout << "Name: " << device.name() << "\n";
+		std::cout << "Rate: " << device.rate() << "\n";
+		std::cout << "State: " << DeviceStateToStr(device.state()) << "\n";
+	}
+	else if (command == "NAME")
+	{
+		if (!device.setName(value))
+		{
+			std::cout << "修改name失败\n";
+		}
+	}
+	else if (command == "RATE")
+	{
+		int rate{};
+		if (parseRate(value, rate))
+		{
+			device.setRate(rate);
+		}
+		else {
+			std::cout << "RATE不合法\n";
+		}
+	}
+	else if (command == "STATE")
+	{
+		DeviceState state{};
+		if (StrToDeviceState(value, state))
+		device.setState(state);
+	}
+	else if (command == "SAMPLES")
+	{
+		const std::array<int, 8> samples{ 120, 118, 121, 125, 119, 123, 122, 120 };
+		int max{ samples[0] }, min{ samples[0] }, sum{};
+		double avg{};
+		for (int sample : samples)
+		{
+			sum += sample;
+			if (sample > max)max = sample;
+			if (sample < min)min = sample;
+		}
+		avg = static_cast<double>(sum) / samples.size();
+		std::cout << "max: " << max << "\n";
+		std::cout << "min: " << min << "\n";
+		std::cout << "avg: " << avg << "\n";
+	}
+
+}
 int main()
 {
-    return 0;
+	DeviceConfig device{};
+	std::cout << "Enter command: ";
+	std::string line{};
+	while (1)
+	{
+		line = getLine();
+		if (line == "EXIT") break;
+		CommandComplier(line, device);
+	}
+	return 0;
 }
--- Day14/02_LeetCode_383_Ransom_Note/main.cpp
+++ Day14/02_LeetCode_383_Ransom_Note/main.cpp
@@ -1,5 +1,27 @@
-#include <array>
+﻿#include <array>
 #include <string>
+
+//使用增强for循环 依次遍历两个字符串   遍历第一个字符串在数组中记录每个字符出现的次数   遍历第二个字符串将对应字符串出现的次数-1   遍历完成后遍历维护的字符出现次数数组，有大于0元素即返回false
+class Solution {
+public:
+    bool canConstruct(std::string ransomNote, std::string magazine) {
+        std::array<int, 26> arr{};
+        for (char value : ransomNote)
+        {
+            arr[value - 'a']++;
+        }
+        for (char value : magazine)
+        {
+            arr[value - 'a']--;
+        }
+        for (int a : arr)
+        {
+            if (a > 0)
+                return false;
+        }
+        return true;
+    }
+};
 
 int main()
 {
