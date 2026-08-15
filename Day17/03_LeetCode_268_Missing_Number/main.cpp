225f52a13a89d4cbc3fb007727d893a1be3b79a1
ghy <ghy@DESKTOP-FN51LQR>
2026-08-15 11:36:14 +0800

    完成Day17代码编写

--- Day17/03_LeetCode_268_Missing_Number/main.cpp
+++ Day17/03_LeetCode_268_Missing_Number/main.cpp
@@ -1,12 +1,21 @@
 #include <vector>
 
+
+//思路：将范围内的所有元素加起来，再和数组中的元素作差，最后剩下的即为数组中未出现的
+
 class Solution {
 public:
-    int missingNumber(std::vector<int>& nums)
-    {
-        // TODO: 在LeetCode完成实现。
-        (void)nums;
-        return 0;
+    int missingNumber(std::vector<int>& nums) {
+        int sum{};
+        for (int i = 0;i <= nums.size();i++)
+        {
+            sum += i;
+        }
+        for (int value : nums)
+        {
+            sum -= value;
+        }
+        return sum;
     }
 };
 
