d5bd54e0528b3c8ee13764920dc63b64bfa5b4d4
hy Guo 郭慧源 <hy.guo@metpeak.com>
2026-08-18 14:49:13 +0800

    完成Day21

--- Day21/02_LeetCode_118_Pascals_Triangle/main.cpp
+++ Day21/02_LeetCode_118_Pascals_Triangle/main.cpp
@@ -1,12 +1,20 @@
-#include <vector>
+﻿#include <vector>
 
 class Solution {
 public:
     std::vector<std::vector<int>> generate(int numRows)
     {
         // TODO: 在LeetCode完成实现。
-        (void)numRows;
-        return {};
+        std::vector<std::vector<int>> nums(numRows);//二维数组的声明
+        for (int i = 0;i < numRows;i++)
+        {
+            nums[i].resize(i + 1, 1);
+            for (int j = 1;j < nums[i].size()-1;j++)
+            {
+                nums[i][j] = nums[i - 1][j] + nums[i - 1][j - 1];
+            }
+        }
+        return nums;
     }
 };
 
